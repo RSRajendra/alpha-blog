639daab1a8f98843ce8e47042c567161cbaf22cd
rajendra <rajendrasinghrana79@gmail.com>
2021-01-22 00:08:02 +0530

    new updates

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_17_145232) do
+ActiveRecord::Schema.define(version: 2021_01_17_165613) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 2021_01_17_145232) do
     t.string "added_by"
     t.string "image"
     t.integer "user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
